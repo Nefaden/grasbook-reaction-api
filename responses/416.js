03c720a715ca746f9799add8469d63d2a395fd64
Yann DURAND <ydurand@4cad.fr>
2019-11-20 12:22:11 +0100

    feat: add 416 HTTP response

---
+++ responses/416.js
@@ -0,0 +1,12 @@
+const responseJson = {
+  '416': {
+    description: 'Requested range unsatisfiable',
+    examples: {
+      'application/json': {
+        message: 'Requested range unsatisfiable'
+      }
+    }
+  }
+};
+
+module.exports = responseJson;
